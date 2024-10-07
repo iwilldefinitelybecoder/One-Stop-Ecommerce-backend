13a2590a5a5e5edb42e3d50047db4d6dace1223a
iwilldefinitelybecoder <tonystarkv9@gmail.com>
2024-10-08 03:41:52 +0530

    production v_1.0.3

--- src/main/java/com/Onestop/ecommerce/ECommerceApplication.java
+++ src/main/java/com/Onestop/ecommerce/ECommerceApplication.java
@@ -20,26 +20,26 @@ public class ECommerceApplication {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
-//	@Autowired
-//	private AuthenticateService authenticateService;
-//	@Autowired
-//	private WareHouseRepo wareHouseRepo;
-//	@Bean
-//	CommandLineRunner runner(){
-//		authenticateService.addRoles("ADMIN");
-//		authenticateService.addRoles("USER");
-//		authenticateService.addRoles("VENDOR");
-//		var wareHouse = WareHouse.builder()
-//				.wareHouseLocation("bangalore")
-//				.wareHouseName("bangalore")
-//				.contactNumber(8315485645L)
-//				.inventory(new ArrayList<>())
-//				.capacity(1000L)
-//				.build();
-//		wareHouseRepo.save(wareHouse);
-//		return args -> {
-//			System.out.println("Application started");
-//		};}
+	@Autowired
+	private AuthenticateService authenticateService;
+	@Autowired
+	private WareHouseRepo wareHouseRepo;
+	@Bean
+	CommandLineRunner runner(){
+		authenticateService.addRoles("ADMIN");
+		authenticateService.addRoles("USER");
+		authenticateService.addRoles("VENDOR");
+		var wareHouse = WareHouse.builder()
+				.wareHouseLocation("bangalore")
+				.wareHouseName("bangalore")
+				.contactNumber(8315485645L)
+				.inventory(new ArrayList<>())
+				.capacity(1000L)
+				.build();
+		wareHouseRepo.save(wareHouse);
+		return args -> {
+			System.out.println("Application started");
+		};}
 
 
 
